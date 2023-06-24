00c4a8bc6c91fbdd84b4213acba1e097f686d626
Berk Tmp <130314590+berktmp@users.noreply.github.com>
2023-06-24 11:57:46 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,15 @@ import com.patikadev.view.LoginGUI;
 
 public class Main {
     public static void main(String[] args) {
-        //yüklü temaları göster
-        /*
-        for(UIManager.LookAndFeelInfo info:  UIManager.getInstalledLookAndFeels()){
-            System.out.println(info.getName()+" => "+info.getClassName());
-        }
+
 
 
         Helper.setLayout();
         Operator operator=new Operator();
         operator.setId(1);
-        operator.setFirstName("yusuf");
-        operator.setLastName("kızılkaya");
-        operator.setUserName("kzlkayay");
+        operator.setFirstName("Berk");
+        operator.setLastName("tmpp");
+        operator.setUserName("berktmp");
         operator.setUserPassword("123456");
         operator.setUserType("OPERATOR");
         OperatorGUI operatorGUI=new OperatorGUI(operator);
